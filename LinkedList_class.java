05d975cb4354edca30653ac496b9ed5d67f1f01a
Anshul  Rajput <94006502+Anshulmayanks16@users.noreply.github.com>
2023-01-08 18:18:21 +0530

    Create LinkedList_class.java

---
+++ LinkedList_class.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
+class my {
+ public static void main (String args[]){
+     LinkedList <String> list  = new LinkedList<String>();
+     list.addFirst("a");
+     list.addFirst("is");
+     System.out.println(list);
+
+
+     list.addFirst("this");
+     list.addLast("list");
+     System.out.println(list);
+     System.out.println(list.size());
+     for(int i = 0; i<list.size();i++){
+         System.out.println(list.get(i)+ "_>");
+     }
+     System.out.println("null");
+     list.remove(3);
+    System.out.println(list);
+ }
+}
