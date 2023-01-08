27f2d404c714a22b4dfa49daa1963fb4fac30855
Anshul  Rajput <94006502+Anshulmayanks16@users.noreply.github.com>
2023-01-08 17:02:25 +0530

    Create add_first_and_last_node.java

---
+++ add_first_and_last_node.java
@@ -0,0 +1,64 @@
+class LL{
+    Node head;
+    class Node {
+        String data;
+        Node next;
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public void addFirst(String data){
+        Node newNode = new Node (data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+
+
+    }
+
+    public void addLast(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+
+        }
+        Node currNode = head;
+        while(currNode.next != null){
+            currNode = currNode.next;
+
+        }
+        currNode.next = newNode;
+    }
+
+    public void printlist(){
+        if(head == null){
+            System.out.println("list is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null){
+            System.out.print(currNode.data + "->");
+            currNode = currNode.next;
+        }
+        System.out.println("Null");
+    }
+
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printlist();
+        list.addLast("list");
+        list.printlist();
+
+    }
+    
+    
+    
+    
+}
